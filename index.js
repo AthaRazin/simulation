6c276e0f190a7051cbdfc164843d0649b88e3ca3
AthaRazin <atharazin18@gmail.com>
2023-12-02 15:34:05 +0700

    commit-pertama

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hellow World");
+  } else if (req.url === "/revou") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hellow Worldasda");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Salah URL");
+  }
+});
+
+const PORT = 3000;
+const IP = "127.0.0.1";
+server.listen(PORT, IP, () => {
+  console.log("Server sudah jalan");
+});
